4717182af6bc1b053f924226456e8faa5eb322d4
EwanJRobertson <73942912+EwanJRobertson@users.noreply.github.com>
2021-12-02 09:49:25 +0000

    Add files via upload

--- databaseOperations.py
+++ databaseOperations.py
@@ -1,37 +1,91 @@
 import sqlite3
 from database import get_db
 
+db = get_db()
+cursor = db.cursor()
+
 # create new user
-def new_user(db, options):
-    db.cursor().execute("INSERT INTO users VALUES(?,?);", options)
+def new_user(options):
+    cursor.execute("""
+        INSERT INTO users VALUES(?,?)
+        ;
+    """, options)
     db.commit()
     return None
 
-# submit password and username, attempt login
-def login(db, username, password):
-    return None
-
 # get all projects that a user is a part of
-def get_projects(db, user_id):
-#   return db.cursor.execute("SELECT project.project_id, project.title, project.version FROM projects JOIN projects_users_link WHERE user_id == '%{user_id}%' GROUP BY project.project_id;".format=(user_id = user_id))
-    return None
+def get_projects(user_id):
+    return cursor.execute("""
+        SELECT project.project_id, project.title, project.version 
+        FROM projects JOIN projects_users_link ON projects.project_id = project_users_link.project_id
+        WHERE user_id == ? 
+        GROUP BY project.project_id
+        ;
+    """, options)
 
 # create new project
 def new_project(options):
-    get_db().cursor().execute('INSERT INTO projects VALUES(?, ?, ?);', options)
-    get_db().commit()
+    cursor.execute("""
+        INSERT INTO projects 
+        VALUES(?, ?, ?)
+        ;
+    """, options)
+    db.commit()
     return None
 
 # get project indo
 def get_project_info(project_id):
-    return get_db().cursor().execute('SELECT * FROM projects;')
+    return cursor.execute("""
+        SELECT * 
+        FROM projects
+        WHERE project_id == ?
+        ;
+    """, options)
 
 # get issues associated with project
-def get_project_issues(db, project_id):
+def get_project_issues(project_id):
+    return cursor.execute("""
+        SELECT *
+        FROM issues
+        WHERE project_id == ?
+        ;
+    """, options)
     return None
 
 # edit existing project
-def edit_project(db, project_id, options):
+def edit_project(options):
+    str = ""
+    for row in cursor.execute("""
+        SELECT *
+        FROM projects
+        WHERE project_id = ?
+        ;
+    """, options):
+        str = row
+    
+    current = str[1, len(str) -2].split(", ")
+    new = []
+    for option in options:
+        if option != "":
+            new.append(option)
+        else:
+            new.append[current[len(new)]]
+    
+    cursor.execute("""
+        DELETE *
+        FROM projects
+        WHERE project_id = ?
+        ;
+    """, options)
+    cursor.commit()
+
+    cursor.execute("""
+        INSERT INTO projects
+        VALUES(?, ?, ?)
+        ;
+        """, options)
+    cursor.commit()
+
     return None
 
 # add user to project
